0a45c7877770daca15063a2c8949a8b06d6edaea
dasecure <vincent@dasecure.com>
2022-11-22 23:23:47 +0000

    Chapter 3.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP port address")
 	flag.Parse()
@@ -14,23 +19,28 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// mux := http.NewServeMux()
+	// fileServer := http.FileServer(http.Dir("../../ui/static/"))
 
-	// mux.HandleFunc("/", home)
-	mux.Handle("/", http.HandlerFunc(home))
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	infoLog.Printf("Starting server on %s", *addr)
+	// mux.HandleFunc("/", app.home)
+	// mux.Handle("/", http.HandlerFunc(home))
+	// mux.HandleFunc("/snippet/view", app.snippetView)
+	// mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
+
+	infoLog.Printf("Starting server on %s", *addr)
 	// err := http.ListenAndServe(*addr, mux)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
