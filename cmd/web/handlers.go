b090e01276734ed1fbb43e0fd30a54ce79969c71
dasecure <vincent@dasecure.com>
2022-11-29 23:09:22 +0000

    Chap7

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/julienschmidt/httprouter"
 	"snippetbox.dasecure.com/internal/models"
 )
 
@@ -15,6 +16,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// log.Println("im at home")
 	// panic("oops! something went wrong!")
 
 	snippets, err := app.snippets.Latest()
@@ -63,13 +65,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 // }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// log.Println("I entered view request")
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		// http.NotFound(w, r)
 		app.notFound(w)
 		return
 	}
 
+	// log.Println(id)
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
@@ -84,54 +90,70 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data.Snippet = snippet
 
 	app.render(w, http.StatusOK, "view.tmpl", data)
-
-	// app.render(w, http.StatusOK, "view.tmpl", &templateData{
-	// 	Snippet: snippet,
-	// })
-
-	// fmt.Fprintf(w, "%+v", snippet)
-
-	// files := []string{
-	// 	"./ui/html/partials/nav.tmpl",
-	// 	"./ui/html/pages/view.tmpl",
-	// 	"./ui/html/base.tmpl",
-	// }
-
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// 	// http.Error(w, "Internal server error", 500)
-	// 	app.serveError(w, err)
-	// 	return
-	// }
-
-	// data := &templateData{
-	// 	Snippet: snippet,
-	// }
-
-	// err = ts.ExecuteTemplate(w, "base", data)
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// 	// http.Error(w, "Internal server error", 500)
-	// 	app.serveError(w, err)
-	// }
 }
 
+// app.render(w, http.StatusOK, "view.tmpl", &templateData{
+// 	Snippet: snippet,
+// })
+
+// fmt.Fprintf(w, "%+v", snippet)
+
+// files := []string{
+// 	"./ui/html/partials/nav.tmpl",
+// 	"./ui/html/pages/view.tmpl",
+// 	"./ui/html/base.tmpl",
+// }
+
+// ts, err := template.ParseFiles(files...)
+// if err != nil {
+// 	log.Println(err.Error())
+// 	// http.Error(w, "Internal server error", 500)
+// 	app.serveError(w, err)
+// 	return
+// }
+
+// data := &templateData{
+// 	Snippet: snippet,
+// }
+
+// err = ts.ExecuteTemplate(w, "base", data)
+// if err != nil {
+// 	log.Println(err.Error())
+// 	// http.Error(w, "Internal server error", 500)
+// 	app.serveError(w, err)
+// }
+
+// func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodPost {
+// 		w.Header().Set("Allow", http.MethodPost)
+// 		// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 		app.clientError(w, http.StatusMethodNotAllowed)
+// 		return
+// 	}
+// 	title := "Slow snail"
+// 	content := "The slow snail was bloody irritating\n, it took so long to get to the finish line.\n\nJack Neo"
+// 	expires := 7
+// 	id, err := app.snippets.Insert(title, content, expires)
+// 	if err != nil {
+// 		app.serveError(w, err)
+// 		return
+// 	}
+// 	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+// 	// w.Write([]byte("Create a new snippet"))
+// }
+
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-	title := "Slow snail"
-	content := "The slow snail was bloody irritating\n, it took so long to get to the finish line.\n\nJack Neo"
+	w.Write([]byte("Display the form for creating a new snippet"))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "Create a new snippet"
+	content := "The slow snail was bloody irritating\n, it took"
 	expires := 7
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serveError(w, err)
 		return
 	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
-	// w.Write([]byte("Create a new snippet"))
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,20 +3,34 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// mux := http.NewServeMux()
+	router := httprouter.New()
 
-	mux.Handle("/", http.HandlerFunc(app.home))
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreate)
+
+	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// mux.Handle("/", http.HandlerFunc(app.home))
+	// mux.HandleFunc("/snippet/view", app.snippetView)
+	// mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return standard.Then(mux)
+	return standard.Then(router)
 }
