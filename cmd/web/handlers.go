6624b06f0e3a5794e457f88d47a2c0ce6ed3aedb
dasecure <vincent@dasecure.com>
2022-12-01 00:40:11 +0000

    Chap8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,11 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.dasecure.com/internal/models"
+	"snippetbox.dasecure.com/internal/validator"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -142,15 +144,81 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 // 	// w.Write([]byte("Create a new snippet"))
 // }
 
+type snippetCreateForm struct {
+	Title   string `form:"title"`
+	Content string `form:"content"`
+	Expires int    `form:"expires"`
+	// FieldErrors map[string]string
+	validator.Validator `form:"-"`
+}
+
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet"))
+	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
+	log.Print("Enter snippet created")
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "Create a new snippet"
-	content := "The slow snail was bloody irritating\n, it took"
-	expires := 7
-	id, err := app.snippets.Insert(title, content, expires)
+	// title := "Create a new snippet"
+	// content := "The slow snail was bloody irritating\n, it took"
+	// expires := 7
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	app.clientError(w, http.StatusBadRequest)
+	// 	return
+	// }
+
+	var form snippetCreateForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// title := r.PostForm.Get("title")
+	// content := r.PostForm.Get("content")
+	// expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	// if err != nil {
+	// 	app.clientError(w, http.StatusBadRequest)
+	// 	return
+	// }
+
+	// form := snippetCreateForm{
+	// 	Title:   r.PostForm.Get("title"),
+	// 	Content: r.PostForm.Get("content"),
+	// 	Expires: expires,
+	// 	// FieldErrors: map[string]string{},
+	// }
+
+	// fieldErrors := make(map[string]string)
+	form.CheckField(validator.NotBlank(form.Title), "title", "Title is required.")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "Title must not exceed 100 characters.")
+	// if strings.TrimSpace(form.Title) == "" {
+	// 	form.FieldErrors["title"] = "This field cannot be empty"
+	// } else if utf8.RuneCountInString(form.Title) > 100 {
+	// 	form.FieldErrors["title"] = "Title cannot be longer than 100 characters"
+	// }
+	form.CheckField(validator.NotBlank(form.Content), "content", "Content cant be empty")
+	// if strings.TrimSpace(form.Content) == "" {
+	// 	form.FieldErrors["content"] = "This field cannot be empty"
+	// }
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must be 1, 7 or 365")
+	// if expires != 1 && expires != 7 && expires != 365 {
+	// 	form.FieldErrors["expires"] = "This field must be 1, 7 or 365"
+	// }
+
+	if !form.Valid() {
+		log.Println("Theres an error")
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serveError(w, err)
 		return
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,7 +22,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
 	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
-	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	// mux.Handle("/", http.HandlerFunc(app.home))
